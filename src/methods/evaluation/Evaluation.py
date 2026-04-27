8524b56b82cbbf1b363e1d9f09b29c839e6ceeb5
Luan <luan.marko2@gmail.com>
2026-04-27 17:39:27 -0300

    [FIX] Adding DINOv2 models to evaluation

--- src/methods/evaluation/Evaluation.py
+++ src/methods/evaluation/Evaluation.py
@@ -13,6 +13,8 @@ from src.methods.simclr.resnet import resnet50 as simclr_resnet50
 from src.methods.swav.resnet import resnet50 as swav_resnet50
 from src.methods.ijepa.models import vit_tiny as ijepa_vit_tiny, vit_small as ijepa_vit_small, vit_base as ijepa_vit_base, \
     vit_large as ijepa_vit_large, vit_huge as ijepa_vit_huge, vit_giant as ijepa_vit_giant
+from src.methods.dinov2.models import vit_tiny as dinov2_vit_tiny, vit_small as dinov2_vit_small, vit_base as dinov2_vit_base, \
+    vit_large as dinov2_vit_large, vit_giant2 as dinov2_vit_giant
 from src.methods.mae.models import mae_vit_base_patch16, mae_vit_large_patch16, mae_vit_huge_patch14, mae_vit_small_patch16, mae_vit_tiny_patch16
 from src.methods.dinov1.models import vit_tiny as dinov1_vit_tiny, vit_small as dinov1_vit_small, vit_base as dinov1_vit_base
 from src.methods.ibot.models import vit_base as ibot_vit_base, vit_large as ibot_vit_large, vit_small as ibot_vit_small, vit_tiny as ibot_vit_tiny
@@ -524,6 +526,47 @@ class Evaluation():
                     except Exception as e:
                         errors.append(("ibot_vit_large_16", str(e)))
                 
+                case "dinov2":
+                    try:
+                        self.encoder = dinov2_vit_tiny(checkpoint=self.meta_checkpoint, patch_size=16)
+                        self.encoder.load_weights(self.evaluate_weights, device=self.device)
+                        self.model_type = "dinov2_vit_tiny_16"
+                        return
+                    except Exception as e:
+                        errors.append(("dinov2_vit_tiny_16", str(e)))
+                    
+                    try:
+                        self.encoder = dinov2_vit_small(checkpoint=self.meta_checkpoint, patch_size=16)
+                        self.encoder.load_weights(self.evaluate_weights, device=self.device)
+                        self.model_type = "dinov2_vit_small_16"
+                        return
+                    except Exception as e:
+                        errors.append(("dinov2_vit_small_16", str(e)))
+                    
+                    try:
+                        self.encoder = dinov2_vit_base(checkpoint=self.meta_checkpoint, patch_size=16)
+                        self.encoder.load_weights(self.evaluate_weights, device=self.device)
+                        self.model_type = "dinov2_vit_base_16"
+                        return
+                    except Exception as e:
+                        errors.append(("dinov2_vit_base_16", str(e)))
+                    
+                    try:
+                        self.encoder = dinov2_vit_large(checkpoint=self.meta_checkpoint, patch_size=16)
+                        self.encoder.load_weights(self.evaluate_weights, device=self.device)
+                        self.model_type = "dinov2_vit_large_16"
+                        return
+                    except Exception as e:
+                        errors.append(("dinov2_vit_large_16", str(e)))
+                    
+                    try:
+                        self.encoder = dinov2_vit_giant(checkpoint=self.meta_checkpoint, patch_size=16)
+                        self.encoder.load_weights(self.evaluate_weights, device=self.device)
+                        self.model_type = "dinov2_vit_giant_16"
+                        return
+                    except Exception as e:
+                        errors.append(("dinov2_vit_giant_16", str(e)))
+
                 case _:
                     errors.append(("unknown_framework", f"Unsupported framework: {self.meta_framework}"))
 
